abac9abcd7492bfd5238c3a506b3e2a37928d8cb
PsyBurn <kresimir.antolic@infinum.hr>
2014-07-20 14:31:34 +0200

    toggle switch

--- app/scripts/game-screens.js
+++ app/scripts/game-screens.js
@@ -36,9 +36,43 @@ _.extend(gameMenuScreen, {
   }
 });
 
+
+var networkGameMenu = new Screen({
+  id: 'network-game-menu'
+});
+
+_.extend(networkGameMenu, {
+  setListeners: function() {
+    this.$el.find('.network-game-start-button').on('click', _.bind(this.onGameStarClick, this));
+    this.$el.find('input[type=radio]').on('change', _.bind(this.onGameTypeChage, this));
+  },
+
+  getGameCodeValue: function() {
+    return this.$el.find('.game-code-input').val();
+  },
+  onGameStarClick: function() {
+    console.log('clicked game start');
+
+  },
+  onGameTypeChage: function() {
+    this.gameType = 'public';
+    var condition = false;
+    if (condition) {
+      this.gameType = 'private';
+    }
+    if (this.gameType === 'public') {
+      this.$el.find('.game-code-input').hide();
+    } else {
+      this.$el.find('.game-code-input').show();
+    }
+  }
+});
+
+
 optionsScreen.init();
 optionsScreen.fillUsername();
 gameMenuScreen.init();
+networkGameMenu.init();
 
 
 
